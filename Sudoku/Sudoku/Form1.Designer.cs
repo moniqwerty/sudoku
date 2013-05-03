73d361407856c8834ee6e8856d7d20df27d7a955
Monika Markovska <monika.markovska93@gmail.com>
2013-05-03 17:44:20 +0200

    first draft
    Splash screen, generate sudoku game algorithm (don't touch sudoku.sc)

---
+++ Sudoku/Sudoku/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.nGameBtn = new System.Windows.Forms.Button();
+            this.settingsBtn = new System.Windows.Forms.Button();
+            this.aboutBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(92, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sudoku!";
+            // 
+            // nGameBtn
+            // 
+            this.nGameBtn.Location = new System.Drawing.Point(101, 90);
+            this.nGameBtn.Name = "nGameBtn";
+            this.nGameBtn.Size = new System.Drawing.Size(136, 28);
+            this.nGameBtn.TabIndex = 1;
+            this.nGameBtn.Text = "New Game";
+            this.nGameBtn.UseVisualStyleBackColor = true;
+            // 
+            // settingsBtn
+            // 
+            this.settingsBtn.Location = new System.Drawing.Point(101, 144);
+            this.settingsBtn.Name = "settingsBtn";
+            this.settingsBtn.Size = new System.Drawing.Size(136, 28);
+            this.settingsBtn.TabIndex = 2;
+            this.settingsBtn.Text = "Settings";
+            this.settingsBtn.UseVisualStyleBackColor = true;
+            // 
+            // aboutBtn
+            // 
+            this.aboutBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.aboutBtn.Location = new System.Drawing.Point(101, 191);
+            this.aboutBtn.Name = "aboutBtn";
+            this.aboutBtn.Size = new System.Drawing.Size(136, 28);
+            this.aboutBtn.TabIndex = 3;
+            this.aboutBtn.Text = "About";
+            this.aboutBtn.UseVisualStyleBackColor = true;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitBtn.Location = new System.Drawing.Point(101, 243);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(136, 28);
+            this.exitBtn.TabIndex = 4;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(354, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.nGameBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.CancelButton = this.exitBtn;
+            this.ClientSize = new System.Drawing.Size(734, 295);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.aboutBtn);
+            this.Controls.Add(this.settingsBtn);
+            this.Controls.Add(this.nGameBtn);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SplashScreen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button nGameBtn;
+        private System.Windows.Forms.Button settingsBtn;
+        private System.Windows.Forms.Button aboutBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label label2;
+
+
+
+    }
+}
+
---
+++ Sudoku/Sudoku/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            Sudoku s = new Sudoku();
+            s.GenerateGame(GameLevel.SIMPLE);
+            int[,] bla = s._numberSet;
+            String str = "";
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    str += bla[i, j];
+                }
+                str += "\n";
+            }
+            label2.Text = str;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
